952f1f796666545fb9b07af72651c701d0e312f1
m-alexandru-91 <alex.m.besleaga@proton.me>
2026-08-15 08:33:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const toPage1Buttons = document.querySelectorAll('.btn-prev-p1');
     const toPage2BackButtons = document.querySelectorAll('.btn-prev-p2');
 
-    toPage2Buttons.forEach(btn => btn.addEventListener('click', () => sec2.scrollIntoView({ behavior: 'smooth' })));
-    toPage3Buttons.forEach(btn => btn.addEventListener('click', () => sec3.scrollIntoView({ behavior: 'smooth' })));
-    toPage1Buttons.forEach(btn => btn.addEventListener('click', () => sec1.scrollIntoView({ behavior: 'smooth' })));
-    toPage2BackButtons.forEach(btn => btn.addEventListener('click', () => sec2.scrollIntoView({ behavior: 'smooth' })));
+    toPage2Buttons.forEach(btn => btn.addEventListener('click', () => sec2.scrollIntoView({ behavior: 'smooth', block: 'center' })));
+    toPage3Buttons.forEach(btn => btn.addEventListener('click', () => sec3.scrollIntoView({ behavior: 'smooth', block: 'center' })));
+    toPage1Buttons.forEach(btn => btn.addEventListener('click', () => sec1.scrollIntoView({ behavior: 'smooth', block: 'center' })));
+    toPage2BackButtons.forEach(btn => btn.addEventListener('click', () => sec2.scrollIntoView({ behavior: 'smooth', block: 'center' })));
 });
